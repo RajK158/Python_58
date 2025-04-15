31fb938f6bb97f9c4d534a60cd70876ab1ceddf5
Raj Kundur <rajkundur.rk@gmail.com>
2025-04-14 22:06:35 -0700

    lists

---
+++ list.py
@@ -0,0 +1,20 @@
+#list
+#set
+#dictionaries
+#tuples
+
+mylist=["Jawa","Python","Sql","Ai_Ml"]
+print(mylist[1:])
+
+myset={"files","documents","data"}
+print(myset)
+
+mydict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(mydict)
+
+mytuple=("flowers","fruits","vegetables")
+print(mytuple)
