fc7404bf10157906df6829d6b81b3a5793745e0c
Raj Kundur <rajkundur.rk@gmail.com>
2025-04-25 20:31:49 -0700

    Simple calc program using functions

---
+++ simple_calc.py
@@ -0,0 +1,14 @@
+a=int(input("Enter number 1: "))
+b=int(input("Enter number 2: "))
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def div(a,b):
+    return a/b
+def mul(a,b):
+    return a*b
+print("add: "+ str(add(a,b)))
+print("sub: "+ str(sub(a,b)))
+print("mul: "+ str(mul(a,b)))
+print("div: "+ str(div(a,b)))
